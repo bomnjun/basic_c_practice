f20ecdca8c3463404f84d6a3ba308f4ac2efec22
bomnjun <bomnjun@gmail.com>
2021-08-13 18:57:39 +0900

    윤년 판독기

---
+++ leapyear.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    scanf("%d", &i);
+
+    if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0) {
+        printf("윤년입니다.\n");
+    }   
+    else { printf("평년입니다.\n"); }
+
+    return 0;
+}
